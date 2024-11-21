19fce51fc31197c7fabd0e5d51d9722122387690
Arvin Angelo C. Arguilles <21-1-00398@vsu.edu.ph>
2024-11-21 23:29:18 +0800

    Latest Working Build
    The app works as intended now!

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,6 +1,9 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
+#include <cstdlib>
+#include <string>
+#include <memory>
 
 #include "flutter_window.h"
 #include "utils.h"
